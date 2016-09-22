36fe25dd10c7efc5cac1de759ee2ef8b8c1abfcb
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-09-22 15:08:36 +0100

    Basic manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,20 @@
-# Class: rpmgpgkey
-# ===========================
-#
-# Full description of class rpmgpgkey here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'rpmgpgkey':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2016 Your name here, unless otherwise noted.
-#
-class rpmgpgkey {
-
-
+# Install an rpm gpg key from a URL
+define rpmgpgkey($ensure, $url) {
+  case $ensure {
+    'present': {
+      exec { "rpmgpgkey present ${name}":
+        command => "/usr/bin/wget -q ${url}/${name} -O /etc/pki/rpm-gpg/${name}",
+        creates => "/etc/pki/rpm-gpg/${name}",
+      }
+    }
+    'absent': {
+      exec { "rpmgpgkey absent ${name}":
+        command => "/bin/rm /etc/pki/rpm-gpg/${name}",
+        unless  => "/bin/ls /etc/pki/rpm-gpg/ | /bin/grep ${name} | /usr/bin/wc -l",
+      }
+    }
+    default: {
+      fail "Invalid 'ensure' value '${ensure}' for rpmgpgkey"
+    }
+  }
 }
